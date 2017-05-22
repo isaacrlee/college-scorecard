11b987b91f765aaf795e707bbe8f940b97a8b284
Jared Fernandez <fern.jared@gmail.com>
2017-05-21 20:49:22 -0500

    Updated Data Set

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import pydotplus, numpy, heapq
-from sklearn, sklearn.neighbors import tree, NearestNeighbors
+from sklearn import tree
+from sklearn.preprocessing import Imputer
+from sklearn.neighbors import NearestNeighbors
 from sklearn.model_selection import cross_val_score
 from data import samples as samples
 from data import target as target
 
+imp = Imputer(missing_values = -1, strategy='mean', axis=0)
+imp.fit(samples)
+samples = imp.transform(samples)
+
+print samples
 # Calculates ZeroR accuracy for Y
 def calc_zeror():
     d = {}
@@ -12,7 +19,7 @@ def calc_zeror():
             d[i] = 1
         else:
             d[i] += 1
-    return float(masamples(i for i in d.values()))/len(target)
+    return float(max(i for i in d.values()))/len(target)
 
 # Prints the Decision Tree (not that helpful)
 def print_tree():
@@ -20,75 +27,75 @@ def print_tree():
     graph = pydotplus.graph_from_dot_data(dot_data) 
     graph.write_pdf("iris.pdf")
 
-# Hamming representations, Euclidean Distance, Normalized (Number of Standard Deviations/Div by Max)
-for attr in range(len(samples[0])):
-    attr_val = []
-    for s in samples:   
-        attr_val.append(s[attr])
-    s_avgs.append(numpy.average(attr_val))
-    s_stds.append(numpy.std(attr_val))
+# # Hamming representations, Euclidean Distance, Normalized (Number of Standard Deviations/Div by Max)
+# for attr in range(len(samples[0])):
+#     attr_val = []
+#     for s in samples:   
+#         attr_val.append(s[attr])
+#     s_avgs.append(numpy.average(attr_val))
+#     s_stds.append(numpy.std(attr_val))
 
-def hamming_kNN(example, k):
-    distance = []
-    for s in range(len(samples)):
-        onehot_d = 0
-        for attr in range(len(s)):
-            if example[attr] != s[attr]:
-                onehot_d += 1 
-        distance.append(onehot_d)
-    min_dsts = heapq.nlargest(k, enumerate(distance), key=lambda x:x[1])
-    classes  = [target[i] for i in min_dsts ] 
-    return max(classes, key = classes.count)
+# def hamming_kNN(example, k):
+#     distance = []
+#     for s in range(len(samples)):
+#         onehot_d = 0
+#         for attr in range(len(s)):
+#             if example[attr] != s[attr]:
+#                 onehot_d += 1 
+#         distance.append(onehot_d)
+#     min_dsts = heapq.nlargest(k, enumerate(distance), key=lambda x:x[1])
+#     classes  = [target[i] for i in min_dsts ] 
+#     return max(classes, key = classes.count)
 
-def L1_kNN(example, k):
-    distance = []
-    for s in range(len(samples)):
-        manhattan_d = 0
-        for attr in range(len(s)):
-            if example[attr] != s[attr]:
-                manhattan_d += np.abs(example[attr] - s[attr])
-        distance.append(manhattan_d)
-    min_dsts = heapq.nlargest(k, enumerate(distance), key=lambda x:x[1])
-    classes  = [target[i] for i in min_dsts ]
-    return max(classes, key = classes.count)
+# def L1_kNN(example, k):
+#     distance = []
+#     for s in range(len(samples)):
+#         manhattan_d = 0
+#         for attr in range(len(s)):
+#             if example[attr] != s[attr]:
+#                 manhattan_d += np.abs(example[attr] - s[attr])
+#         distance.append(manhattan_d)
+#     min_dsts = heapq.nlargest(k, enumerate(distance), key=lambda x:x[1])
+#     classes  = [target[i] for i in min_dsts ]
+#     return max(classes, key = classes.count)
 
-def L2_kNN(example, k):
-    distance = []
-    for s in range(len(samples)):
-        euclid_d = 0
-        for attr in range(len(s)):
-            if example[attr] != s[attr]:
-                euclid_d += (example[attr] - s[attr]) ** 2 
-        distance.append(numpy.sqrt(euclid_d))
-    min_dsts = heapq.nlargest(k, enumerate(distance), key=lambda x:x[1])
-    classes  = [target[i] for i in min_dsts ]
-    return max(classes, key = classes.count)
+# def L2_kNN(example, k):
+#     distance = []
+#     for s in range(len(samples)):
+#         euclid_d = 0
+#         for attr in range(len(s)):
+#             if example[attr] != s[attr]:
+#                 euclid_d += (example[attr] - s[attr]) ** 2 
+#         distance.append(numpy.sqrt(euclid_d))
+#     min_dsts = heapq.nlargest(k, enumerate(distance), key=lambda x:x[1])
+#     classes  = [target[i] for i in min_dsts ]
+#     return max(classes, key = classes.count)
 
-def normalized_L2(example, k):
-    distance = []
-    averages = []
-    std_devs = []
+# def normalized_L2(example, k):
+#     distance = []
+#     averages = []
+#     std_devs = []
     
-    for i in range(len(samples[0])):
-        for j in samples:
+#     for i in range(len(samples[0])):
+#         for j in samples:
 
-    for s in range(len(samples)):
-        euclid_d = 0
-        for attr in range(len(s)):
-            if example[attr] != s[attr]:
-                euclid_d += (example[attr] - s[attr]) ** 2 
-        distance.append(numpy.sqrt(euclid_d))
-    min_dsts = heapq.nlargest(k, enumerate(distance), key=lambda x:x[1])
-    classes  = [target[i] for i in min_dsts ]
-    return max(classes, key = classes.count)
+#     for s in range(len(samples)):
+#         euclid_d = 0
+#         for attr in range(len(s)):
+#             if example[attr] != s[attr]:
+#                 euclid_d += (example[attr] - s[attr]) ** 2 
+#         distance.append(numpy.sqrt(euclid_d))
+#     min_dsts = heapq.nlargest(k, enumerate(distance), key=lambda x:x[1])
+#     classes  = [target[i] for i in min_dsts ]
+#     return max(classes, key = classes.count)
 
-def tester():
+# def tester():
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     dtree_clf = tree.DecisionTreeClassifier()
     dtree_clf = dtree_clf.fit(samples, target)
-    dtree_scores = cross_val_score(dtree_clf, samples, target, cv=4)
+    dtree_scores = cross_val_score(dtree_clf, samples, target, cv=10)
 
     print ("ZeroR Accuracy: %0.2f" % (calc_zeror()))
     print("Tree Accuracy: %0.2f (+/- %0.2f)" % (dtree_scores.mean(), dtree_scores.std() * 2))
